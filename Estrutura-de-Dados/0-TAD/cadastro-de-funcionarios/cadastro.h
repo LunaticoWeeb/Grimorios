c7f5944c3ad1602e78d8b15a46c22640ed64ae87
LunaticoWeeb <2sa4vini@gmail.com>
2023-11-03 18:20:19 -0300

    Criado exemplo de TAD: cadastro de funcionários

--- Estrutura-de-Dados/0-TAD/cadastro-de-funcionarios/cadastro.h
+++
@@ -1,6 +0,0 @@
-#ifndef cadastro_h
-#define cadastro_h
-
-#include <stdio.h>
-
-#endif //CADASTRO_H
--- Estrutura-de-Dados/0-TAD/cadastro-de-funcionarios/funcionario.h
+++ Estrutura-de-Dados/0-TAD/cadastro-de-funcionarios/funcionario.h
@@ -1,10 +1,21 @@
 #ifndef funcionario_h
 #define funcionario_h
 
+#define VAZIO -1
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct funcionario_t FUNCIONARIO;
 
 FUNCIONARIO *novo_funcionario(char nome[100], char cargo[100], float salario);
+void apagar_funcionario(FUNCIONARIO* funcionario);
+
+void alterar_salario(FUNCIONARIO* funcionario, float novo_salario);
+void alterar_cargo(FUNCIONARIO* funcionario, char novo_cargo[100], float novo_salario);
+void alterar_nome(FUNCIONARIO* funcionario, char novo_nome[100]);
+
+void imprimir_funcionario(FUNCIONARIO* funcionario);
 
 #endif //FUNCIONARIO_H
