0372c202104373f3a0211357fc173ea6d9e8f54d
LunaticoWeeb <2sa4vini@gmail.com>
2023-11-09 08:41:40 -0300

    First commit on Bash

---
+++ Bash/Programas/2-variaveis.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+NOME=Luna
+SISTEMA=Melchior
+
+echo Olá $NOME bem-vinda ao sistema $SISTEMA!
---
+++ Bash/Programas/4-arg-posicionais.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+NOME=$1
+SISTEMA=$2
+
+echo Olá $NOME bem-vinde ao sistema $SISTEMA!
